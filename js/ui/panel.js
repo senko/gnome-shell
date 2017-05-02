49849f1bd2df558202464d8630a571f25a9395bc
Mario Sanchez Prada <mario@endlessm.com>
2017-05-02 20:31:37 +0100

    Add Endless' Hot Corner to the right side of the bottom panel
    This patch includes the new resources required as well as new GSettings
    keys to allow moving the hot corner to any other corner on the screen.

--- js/ui/panel.js
+++ js/ui/panel.js
@@ -782,6 +782,7 @@ const PANEL_ITEM_IMPLEMENTATIONS = {
     'a11y': imports.ui.status.accessibility.ATIndicator,
     'a11yGreeter': imports.ui.status.accessibility.ATGreeterIndicator,
     'keyboard': imports.ui.status.keyboard.InputSourceIndicator,
+    'hotCorner': imports.ui.hotCorner.HotCorner,
 };
 
 const Panel = new Lang.Class({
