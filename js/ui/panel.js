432966a15d4a340ecc240aafbd1a2d0044495665
Cosimo Cecchi <cosimo@endlessm.com>
2017-06-08 14:44:17 -0700

    system: move orientation to the system settings menu
    Instead of using a button in the user menu, move it to be a row in the
    system settings.
    
    https://phabricator.endlessm.com/T17488

--- js/ui/panel.js
+++ js/ui/panel.js
@@ -725,6 +725,7 @@ const AggregateMenu = new Lang.Class({
         this._screencast = new imports.ui.status.screencast.Indicator();
         this._location = new imports.ui.status.location.Indicator();
         this._nightLight = new imports.ui.status.nightLight.Indicator();
+        this._orientation = new imports.ui.status.orientation.Indicator();
 
         this._indicators.add_child(this._screencast.indicators);
         this._indicators.add_child(this._location.indicators);
@@ -763,6 +764,7 @@ const AggregateMenu = new Lang.Class({
             this.menu.addMenuItem(this._bluetooth.menu);
         }
         this.menu.addMenuItem(this._location.menu);
+        this.menu.addMenuItem(this._orientation.menu);
         this.menu.addMenuItem(this._rfkill.menu);
         this.menu.addMenuItem(this._power.menu);
         this.menu.addMenuItem(this._nightLight.menu);
@@ -773,6 +775,7 @@ const AggregateMenu = new Lang.Class({
         this.menu.addMenuItem(this._brightness.menu);
 
         menuLayout.addSizeChild(this._location.menu.actor);
+        menuLayout.addSizeChild(this._orientation.menu.actor);
         menuLayout.addSizeChild(this._rfkill.menu.actor);
         menuLayout.addSizeChild(this._power.menu.actor);
     },
