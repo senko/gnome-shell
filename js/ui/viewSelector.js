1c646e104b39444e2a7cbc3fdd7c2bc7804b0966
Mario Sanchez Prada <mario@endlessm.com>
2017-06-14 14:24:18 +0100

    viewSelector: Allow triggering the desktop search on key presses
    This implements the necessary hooks to start searching when typing
    from the APPS page (icon grid mode).

--- js/ui/viewSelector.js
+++ js/ui/viewSelector.js
@@ -578,8 +578,15 @@ const ViewSelector = new Lang.Class({
         Main.overview.show();
     },
 
+    _clearSearch: function() {
+        this._entry.resetSearch();
+        this._viewsDisplay.actor.showPage(ViewsDisplayPage.APP_GRID, false);
+    },
+
     show: function(viewPage) {
         this._activePage = null;
+        this._clearSearch();
+
         this._showPage(this._pageFromViewPage(viewPage));
         this._workspacesDisplay.show(true);
     },
@@ -603,6 +610,11 @@ const ViewSelector = new Lang.Class({
 
     hide: function() {
         // Nothing to do, since we always show the app selector
+    },
+
+    focusSearch: function() {
+        if (this._activePage == this._appsPage)
+            this._entry.grab_key_focus();
     },
 
     _addPage: function(actor, name, a11yIcon, params) {
@@ -679,6 +691,13 @@ const ViewSelector = new Lang.Class({
         }
     },
 
+    _pageChanged: function() {
+        if (this._activePage != this._appsPage)
+            this._clearSearch();
+
+        this.emit('page-changed');
+    },
+
     _showPage: function(page) {
         if (!Main.overview.visible)
             return;
@@ -688,7 +707,7 @@ const ViewSelector = new Lang.Class({
 
         let oldPage = this._activePage;
         this._activePage = page;
-        this.emit('page-changed');
+        this._pageChanged();
 
         if (oldPage)
             this._animateOut(oldPage)
@@ -715,6 +734,12 @@ const ViewSelector = new Lang.Class({
             }
             return Clutter.EVENT_PROPAGATE;
         }
+
+        if (this._entry.handleStageEvent(event))
+            return Clutter.EVENT_STOP;
+
+        if (this._entry.active)
+            return Clutter.EVENT_PROPAGATE;
 
         if (!global.stage.key_focus) {
             if (symbol == Clutter.Tab || symbol == Clutter.Down) {
